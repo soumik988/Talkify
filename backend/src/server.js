00ef4607da9ac615116fea7266d0bef752115e3b
iamsoumik <153822678+soumik988@users.noreply.github.com>
2025-10-07 19:28:00 +0530

    fix some issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,35 +1,27 @@
-//const express = require('express');
 import express from "express";
-import dotenv from "dotenv"
-import path from 'path';
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-import { connectDB } from "./lib/db.js";
+import dotenv from "dotenv";
+import path from "path";
+import connectDB from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())  //req.body
+connectDB(); // Connect to MongoDB
 
-const __dirname = path.resolve();
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-
-app.use('/api/auth', authRoutes)
-app.use('/api/messages', messageRoutes)
-
-//make ready for deployment
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get('*', (_, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
-
-
-app.listen(PORT, () => {
-    console.log('Server is running on port : ' + PORT);
-    connectDB()
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
